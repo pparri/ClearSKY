6157f2b135a5a16328bddeb8b1494ee6bcffacfa
Axel Gonzalez <axel.david.gonzalez@estudiantat.upc.edu>
2025-04-08 15:47:47 +0200

    config db

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,3 +4,13 @@ REST_FRAMEWORK = {  # Enable JWT token for auth
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clearsky',          # Nombre de tu base de datos
+        'USER': 'clearsky',          # Usuario de la base de datos
+        'PASSWORD': 'clearsky', # Contraseña del usuario
+        'HOST': 'localhost',         # Dirección del servidor (o 'postgres' si usas Docker)
+        'PORT': '5432',              # Puerto de PostgreSQL (por defecto es 5432)
+    }
+}
